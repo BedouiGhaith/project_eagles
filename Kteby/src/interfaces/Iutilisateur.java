bec250a6fea75ec9e71eee424af423fa6aa82988
BedouiGhaith <98600281+BedouiGhaith@users.noreply.github.com>
2022-02-24 02:01:02 +0100

    Revert "a"
    This reverts commit 0650ffe1ff5b1fad124bb0dbc1ac3e3bd796bf3f.

---
+++ Kteby/src/interfaces/Iutilisateur.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+import java.util.List;
+import model.utilisateur;
+
+/**
+ *
+ * @author asus
+ */
+public interface Iutilisateur {
+     public void ajouterUtilisateur(utilisateur u);
+   public void deleteUtilisateur(utilisateur s);
+   public void updateUtilisateur(utilisateur u,utilisateur s);
+   public List<utilisateur> consulterUtilisateur();
+   public utilisateur getUserById(utilisateur u);
+   public utilisateur getUserByEmail(utilisateur u);
+   public List<utilisateur> getUserByName(utilisateur u);
+   public utilisateur getUserByUsername(utilisateur u);
+   public List<utilisateur> getUserByAge(utilisateur u);
+}
