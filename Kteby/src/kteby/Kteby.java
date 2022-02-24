a3d4102965f5cc45a6105db2b60faf202057c00c
lahmaroussama <98613514+lahmaroussama@users.noreply.github.com>
2022-02-24 03:55:09 +0100

    Update Kteby.java

--- Kteby/src/kteby/Kteby.java
+++ Kteby/src/kteby/Kteby.java
@@ -6,15 +6,17 @@
 package kteby;
 
 
+import interfaces.Iclub;
+import interfaces.Ilivre;
 import model.club;
 
 import model.livre;
+import model.members;
 
 
 import services.serviceClub;
-
 import services.serviceLivre;
-
+import services.serviceMembers;
 
 /**
  *
@@ -27,34 +29,50 @@ public class Kteby {
      */
     public static void main(String[] args) {
         
-        
-        
-        //books
-        livre li = new livre("na","na","na","na",5);
-        livre updated_livre = new livre("a","a","a","a",5);
-        
-        serviceLivre sli = new serviceLivre();
-        
-        sli.ajouterLivre(li);
-        sli.updateLivre(updated_livre,"na");
-        System.out.println(sli.consulterLivre());
-        sli.deleteLivre("na");
-        
-        
        
         
+      /*  //books
+        livre li = new livre("harrypoter","pablo","heloothere","image",5);
+        livre updated_livre = new livre("willsmith","pawlo","dracula","image",5);
+        
+        Ilivre sli = new serviceLivre();
+         
+        System.out.println(sli.calculelivre());
+        sli.ajouterLivre(li);
+       
+        System.out.println(sli.consulterLivre());
+        sli.deleteLivre(li);
+        System.out.println(sli.getLivreById(sli.consulterLivre().get(0)));
+        
+        */
         //club
-        club cl = new club("club","22/22/2222","me",20);
-        club cl = new club("club","10/10/1111","me",scl.calculemembre());
+        Iclub scl = new serviceClub();
         
-        serviceClub scl = new serviceClub();
+        club c = new club("club","10/10/1111","me",scl.calculemembre());
+        club updated_cl = new club("club1","11/11/1111","he",scl.calculemembre());
+        //club cl = new club();
         
-        scl.ajouterClub(cl);
-        scl.updateClub(updated_cl, "club");
-        System.out.println(scl.consulterClub());
-        scl.deleteClub("club123");
+        System.out.println(scl.calculemembre()); 
+      
+        scl.ajouterClub(c);
+       
+        
+        scl.deleteClub(c);
+        //System.out.println(scl.consulterClub());
+      //  scl.updateClub(updated_cl,c);
+        System.out.println(scl.getClubById(scl.consulterClub().get(0)));
+        
+       //members
+        
+       /* serviceMembers me = new serviceMembers() ;
+        members m1 = new members(4,3);
+        members m2 = new members(5,5);
+        me.ajouterMember(m1);
+        me.ajouterMember(m2);
+        System.out.println(scl.calculemembre());*/
         
       
+        
         
     }
 
